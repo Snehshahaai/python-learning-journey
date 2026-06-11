eba7a85069900abc9c5af434563fe8b435affafc
Sneh Shah <snehshah@Snehs-MacBook-Air.local>
2026-06-12 00:59:52 +0530

    Remove duplicate pandas files from root

--- pandas_challenge3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.DataFrame({
-    "Name": ["Rahul", "Priya", "Amit"],
-    "Marks": [85, 92, 78]
-})
-df["Result"] = np.where(df["Marks"] >= 80, 'Pass','Fail')
-print(df)
